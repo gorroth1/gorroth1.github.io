f79cb88d30fb38a8f6495540c7874e5c1ddc2904
Gabe Orroth <gorroth1@gmail.com>
2016-11-02 20:59:54 -0400

    save

---
+++ dodge/gab.js
@@ -0,0 +1,27 @@
+  class Boot {
+      preload() {
+          this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+          this.scale.pageAlignHorizontally = true;
+          this.scale.pageAlignVertically = true;
+        }
+    
+    create() {
+      this.state.start("Load")
+    }
+  }
+
+  class Load {
+    preload() {
+      console.log("Loading...");
+      this.load.image() 
+    }
+  create() {
+    console.log("Loaded");
+  }
+}
+var game = new Phaser.Game(320,568);  
+game.state.add("Boot",Boot);
+game.state.add("Load",Load);
+game.state.start("Boot");
+
+
