ecbbb81131fe20b1631fa064e04d52aab3c6ded4
Gabe Orroth <gorroth1@gmail.com>
2016-12-14 21:07:02 -0500

    save

--- dodge/gab.js
+++ dodge/gab.js
@@ -15,18 +15,20 @@ var C = {
     "width": 32,
     "height": 32,
     "frames": 2,
+    "fps": 3,
     "startx": 160,
     "starty": 500,
     "speed": 5
   },
   "d": {
-    "file": "assets/dodge.png",
-    "width": 64,
-    "hright": 64,
-    "frames": 2,
+    "file": "assets/poop.png",
+    "width": 32,
+    "height": 32,
+    "frames": 4,
     "fps": 10,
     "startx": 160,
-    "starty": 32
+    "starty":-32,
+    "speed": 5
   }
 }
 
@@ -47,7 +49,7 @@ var C = {
       console.log("Loading...");
       this.load.image("bg","assets/background.png") 
       this.load.spritesheet("player",C.p.file,C.p.width,C.p.height,C.p.frames);
-      //this.load.spritesheet("dodge",C.d.file,C.d.width,C.d.height,C.d.frames);
+      this.load.spritesheet("poop",C.d.file,C.d.width,C.d.height,C.d.frames);
     }
   create() {
     console.log("Loaded");
@@ -65,17 +67,17 @@ class Play {
     this.player = this.add.sprite(C.p.startx,C.p.starty,"player");
     this.player.anchor.set(0.5,0.5);
     this.player.smoothed = false;
-    this.player.scale.set(1);
+    this.player.scale.set(2);
     this.player.animations.add("anim");
     this.player.animations.play("anim",C.p.fps,true);
-/*
-    this.gab = this.add.sprite(C.d.startx,C.d.starty,"dodge");
+    console.log(this.player)
+    
+    this.gab = this.add.sprite(C.d.startx,C.d.starty,"poop");
     this.gab.anchor.set(0.5,0.5);
     this.gab.smoothed = false;
-    this.gab.scale.set(1);
+    this.gab.scale.set(2);
     this.gab.animations.add("anim");
     this.gab.animations.play("anim",C.d.fps,true);
-*/
     this.cursors = this.input.keyboard.createCursorKeys();
   }
   update(){
@@ -84,18 +86,42 @@ class Play {
     }
     if (this.cursors.right.isDown) {
       this.player.x += C.p.speed;
+       
+    }
+    if (this.gab.y > this.game.height) {
+      this.gab.y = C.d.starty
+      let px = (C.d.width * this.gab.scale.x) / 2;
+      let max = C.game.width - px
+      let min = 0 + px
+      let newx = randInt(max);
+      if (newx < min){ newx = min }
+      this.gab.x = newx
+    }
+    if (this.gab.y == C.d.starty) {
+    }
+    this.gab.y += C.d.speed;
+  }
+
+
+    render() {
+      game.debug.text("x: " + this.gab.x + ", y: " + this.gab.y, 4, 16); 
     }
   }
-}
 
-function restart() {
+
+    function restart() {
   game.state.start("Boot");
-}
+    }
+
+    function randInt(max) {
+      return Math.floor(Math.random() * max);
+    }
+
 
 var game = new Phaser.Game(320,568);  
 game.state.add("Boot",Boot);
 game.state.add("Load",Load);
 game.state.add("Play",Play);
 game.state.start("Boot");
-
-
+  
+  
